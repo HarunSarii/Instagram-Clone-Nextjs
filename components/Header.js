3ed5c048879773d7dad5da01c16533485ec408e1
HarunSarii <harunsari14@gmail.com>
2022-08-15 23:01:37 +0300

    add session data to Header

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,11 @@ import Image from 'next/image'
 import React from 'react'
 import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, MenuIcon, PaperAirplaneIcon } from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 
 function Header() {
+    const { data: session } = useSession();
 
     return (
         <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
@@ -39,19 +41,25 @@ function Header() {
                 <div className="flex items-center justify-end space-x-4">
                     <HomeIcon className="navBtn" />
                     <MenuIcon className="h-6 cursor-pointer md:hidden" />
-                    <div className='relative navBtn' >
-
-                    <PaperAirplaneIcon className="navBtn rotate-45 "  />
-                    <div className='absolute -top-2 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'  >3</div>
-                    </div>
-                    <PlusCircleIcon className="navBtn" />
-                    <UserGroupIcon className="navBtn" />
-                    <HeartIcon className="navBtn" />
-                    <img
-                        src="https://qph.cf2.quoracdn.net/main-thumb-54497310-50-qhckqvfhjsrbsktgjqawefyxgxvzuqzr.jpeg" 
-                        alt='profile pic'
-                        className='rounded-full h-10 cursor-pointer'
-                    />
+                    {session ? (
+                        <>
+                            <div className='relative navBtn' >
+                                <PaperAirplaneIcon className="navBtn rotate-45 " />
+                                <div className='absolute -top-2 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'  >3</div>
+                            </div>
+                            <PlusCircleIcon className="navBtn" />
+                            <UserGroupIcon className="navBtn" />
+                            <HeartIcon className="navBtn" />
+                            <img
+                                onClick={signOut }
+                                src={session.user?.image}
+                                alt='profile pic'
+                                className='rounded-full h-10 cursor-pointer'
+                            />
+                        </>
+                    ) : (
+                        <button onClick={signIn} >Sign In</button>
+                    )}
                 </div>
 
             </div>
