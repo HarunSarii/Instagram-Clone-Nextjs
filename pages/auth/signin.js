746bb9f1709e6e536eda65d84e84d947be037538
HarunSarii <harunsari14@gmail.com>
2022-08-15 21:23:46 +0300

    add signin

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { getProviders, signIn as SignIntoProviders } from "next-auth/react"
 
-function signIn() {
+export default function signIn({ providers }) {
+  console.log('providerss', providers);
   return (
-    <div>signIn</div>
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => SignIntoProviders(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
   )
 }
 
-export 
-
-export default signIn
+export async function getServerSideProps(context) {
+  const providers = await getProviders()
+  console.log("providers", providers);
+  return {
+    props: { providers },
+  }
+}
