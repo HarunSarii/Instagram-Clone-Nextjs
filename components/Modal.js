73fb5b0c0e665370c704d05279b8f51eaeec56a6
HarunSarii <harunsari14@gmail.com>
2022-08-18 18:57:37 +0300

    add upload photo and post sections

--- components/Modal.js
+++ components/Modal.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition } from '@headlessui/react';
+import { CameraIcon } from '@heroicons/react/outline';
 
 
 const Modal = () => {
@@ -28,10 +29,10 @@ const Modal = () => {
           </Transition.Child>
 
           <span
-            className='hidden-sm:inline-block sm:align-middle sm:h-screen'
-            aria-hidden='true'
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
           >
-            &#8203
+            &#8203;
           </span>
 
           <Transition.Child
@@ -43,14 +44,56 @@ const Modal = () => {
             leavefrom='opacity-100 translate-y-0 sm:scale-100'
             leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
           >
-            <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left 
-                            overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 '>
-              <h1>hello</h1>
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden
+                            shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+              <div>
+                <div
+                  className='mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer'
+                >
+                  <CameraIcon
+                    className='h-6 w-6 text-red-600'
+                    aria-hidden='true'
+                  />
+                </div>
+                <div>
+                  <div className='mt-3 text-center sm:mt-5' >
+                    <Dialog.Title
+                      as='h3'
+                      className='text-lg leading-6 font-medium text-gray-900'
+                    >
+                      Upload a photo
+                    </Dialog.Title>
+                    <div>
+                      <input
+                        type='file'
+                        hidden
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type='text'
+                        className='border-none focus:ring-0 w-full text-center'
+                        placeholder='Please enter a caption...'
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className='mt-5 sm:mt-6'>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm
+                                px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
+                                focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300
+                                disabled:cursor-not-allowed hover:disabled:bg-gray-300">
+                    Upload Post
+                  </button>
+                </div>
+              </div>
             </div>
           </Transition.Child>
         </div>
-      </Dialog>
-    </Transition.Root>
+      </Dialog >
+    </Transition.Root >
   )
 }
 
